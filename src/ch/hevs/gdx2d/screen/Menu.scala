d4c16771dfdff764f2732ae2e3f26d7d89638068
Algath <119356079+Algath@users.noreply.github.com>
2024-06-04 16:42:56 +0200

    premier bouton fait
    Took 31 minutes

--- src/ch/hevs/gdx2d/screen/Menu.scala
+++ src/ch/hevs/gdx2d/screen/Menu.scala
@@ -1,12 +1,36 @@
 package ch.hevs.gdx2d.screen
 
 import ch.hevs.gdx2d.components.screen_management.RenderingScreen
-import ch.hevs.gdx2d.lib.GdxGraphics
+import ch.hevs.gdx2d.game.Handler
+import ch.hevs.gdx2d.lib.{GdxGraphics, ScreenManager}
 import ch.hevs.gdx2d.main.Main
+import ch.hevs.gdx2d.main.Main.s
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.ui.{Skin, TextButton}
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.{InputEvent, Stage}
 
 class Menu extends RenderingScreen {
   override def onInit(): Unit = {
+    var stage = new Stage()
+    Gdx.input.setInputProcessor(stage)
 
+    var skin = new Skin(Gdx.files.internal("data/ui/uiskin.json"))
+    var singlePlayer: TextButton = new TextButton("Single Player", skin)
+    singlePlayer.setWidth(180)
+    singlePlayer.setHeight(30)
+    singlePlayer.setPosition(Gdx.graphics.getWidth/2 - 180 / 2, (Gdx.graphics.getHeight * 0.6).toInt)
+
+    stage.addActor(singlePlayer)
+
+    singlePlayer.addListener(new ClickListener() {
+      override def clicked(event: InputEvent, x: Float, y: Float): Unit = {
+        super.clicked(event, x, y)
+
+        if (singlePlayer.isChecked)
+          Start()
+      }
+    })
   }
 
   var time:Float = 0
@@ -24,6 +48,24 @@ class Menu extends RenderingScreen {
 
     //gdxGraphics.drawCircle(500, 500, 100)
 
+  }
+
+  def Start(): Unit = {
+    Handler.Init()
+    Handler.InitPlayer(1, false)
+    s.transitionTo(1, ScreenManager.TransactionType.SLICE)
+  }
+
+  def StartMultiplaying(): Unit = {
+    Handler.Init()
+    Handler.InitPlayer(2, false)
+    s.transitionTo(1, ScreenManager.TransactionType.SLICE)
+  }
+
+  def StartVersus(): Unit = {
+    Handler.Init()
+    Handler.InitPlayer(2, true)
+    s.transitionTo(2, ScreenManager.TransactionType.SLICE)
   }
 
 }
